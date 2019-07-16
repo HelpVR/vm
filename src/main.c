6f1f5d1f559a454b32a4d9859279b8563e5f26bf
George Fotopoulos <xorz57@protonmail.com>
2019-07-16 13:14:07 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,12 +2,12 @@
 #include "execute.h"
 #include "signal.h"
 
-bool running = true;
+bool running;
 
 uint16_t mem[ADDRESSES];
 uint16_t reg[REGISTERS];
 
-uint16_t pc = 0x0000;
+uint16_t pc;
 
 uint16_t fetch() { return mem[pc++]; }
 
@@ -23,9 +23,12 @@ int main() {
   mem[0x3002] = 0x2201;
   mem[0x3003] = 0x0000;
 
+  running = true;
+
   while (running) {
     uint16_t instruction = fetch();
     execute(instruction);
   }
+
   return 0;
 }
